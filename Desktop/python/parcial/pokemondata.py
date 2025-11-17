4458aa9f1fedcfa0afae7ff84352bd7fbbd4d2e1
gonzafc <romerogonzalo32@gmail.com>
2025-11-17 20:51:48 -0300

    Ejercicios del parcial 2

---
+++ Desktop/python/parcial/pokemondata.py
@@ -0,0 +1,14 @@
+pokemon_list = [
+    {'name': 'Bulbasaur', 'number': 1, 'types': ['Grass', 'Poison'], 'weaknesses': ['Fire', 'Ice', 'Flying', 'Psychic'], 'mega': False, 'gigamax': True},
+        {'name': 'Charmander', 'number': 4, 'types': ['Fire'], 'weaknesses': ['Water', 'Ground', 'Rock'], 'mega': False, 'gigamax': True},
+        {'name': 'Charizard', 'number': 6, 'types': ['Fire', 'Flying'], 'weaknesses': ['Water', 'Electric', 'Rock'], 'mega': True, 'gigamax': True},
+        {'name': 'Pikachu', 'number': 25, 'types': ['Electric'], 'weaknesses': ['Ground'], 'mega': False, 'gigamax': True},
+        {'name': 'Gengar', 'number': 94, 'types': ['Ghost', 'Poison'], 'weaknesses': ['Ground', 'Psychic', 'Ghost', 'Dark'], 'mega': True, 'gigamax': True},
+        {'name': 'Jolteon', 'number': 135, 'types': ['Electric'], 'weaknesses': ['Ground'], 'mega': False, 'gigamax': False},
+        {'name': 'Steelix', 'number': 208, 'types': ['Steel', 'Ground'], 'weaknesses': ['Fire', 'Water', 'Fighting', 'Ground'], 'mega': True, 'gigamax': False},
+        {'name': 'Lycanroc', 'number': 745, 'types': ['Rock'], 'weaknesses': ['Water', 'Grass', 'Fighting', 'Ground', 'Steel'], 'mega': False, 'gigamax': False},
+        {'name': 'Tyrantrum', 'number': 697, 'types': ['Rock', 'Dragon'], 'weaknesses': ['Ice', 'Fighting', 'Ground', 'Steel', 'Dragon', 'Fairy'], 'mega': False, 'gigamax': False},
+        {'name': 'Bullywug', 'number': 1026, 'types': ['Fighting'], 'weaknesses': ['Flying', 'Psychic', 'Fairy'], 'mega': False, 'gigamax': False},
+        {'name': 'Ivysaur', 'number': 2, 'types': ['Grass', 'Poison'], 'weaknesses': ['Fire', 'Ice', 'Flying', 'Psychic'], 'mega': False, 'gigamax': False},
+        {'name': 'Venusaur', 'number': 3, 'types': ['Grass', 'Poison'], 'weaknesses': ['Fire', 'Ice', 'Flying', 'Psychic'], 'mega': True, 'gigamax': True},
+]
